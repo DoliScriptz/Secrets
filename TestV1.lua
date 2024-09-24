4e31e33d3f04f89214f9a44e5b85a1397a789b48
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2024-09-24 15:36:15 +0700

    Update TestV1.lua

--- TestV1.lua
+++ TestV1.lua
@@ -2,4 +2,4 @@ getgenv().gui = false
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DoliScriptz/Secrets/main/main.lua"))()
 
-loadstring(game:HttpGet("https://egorikusa.space/0067dfd7ec316ff2d6446c5f.lua", true))()
+loadstring(game:HttpGet("https://egorikusa.space/1541aff2091eb852379b2aa5.lua", true))()
